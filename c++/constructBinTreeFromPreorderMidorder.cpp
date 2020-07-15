84636a0477211b66e9e25acc6a71b8e31df97e39
“zhaoccgit config --global user.email “450959507@qq.com <450959507@qq.com>
2020-07-16 06:52:16 +0800

    Add more.

---
+++ c++/constructBinTreeFromPreorderMidorder.cpp
@@ -0,0 +1,50 @@
+/**
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    static unordered_map<int, int> index;
+
+    static TreeNode *myBuildTree(const vector<int> &preorder, const vector<int> &inorder, int leftPreorder,
+                                 int rightPreorder, int leftInorder, int rightInorder) {
+        if (leftPreorder > rightPreorder) {
+            return NULL;
+        }
+
+        int preorderRootVal = preorder[leftPreorder]; // 根节点的值
+        int inorderRootInd = index[preorderRootVal]; // 根节点在中序的位置
+
+        TreeNode *inorderRoot = new TreeNode(preorderRootVal); // 构建根节点
+
+        int sizeOfLeftTreePreorder = inorderRootInd - leftInorder; // 左子树前序数组的长度
+
+        // 递归构建左子树
+        inorderRoot->left = myBuildTree(preorder, inorder, leftPreorder + 1, leftPreorder + sizeOfLeftTreePreorder,
+                leftInorder, inorderRootInd - 1);
+        // 递归构建右子树
+        inorderRoot->right = myBuildTree(preorder, inorder, leftPreorder + sizeOfLeftTreePreorder + 1, rightPreorder,
+                inorderRootInd + 1, rightInorder);
+        return inorderRoot;
+    }
+
+    static TreeNode *buildTree(const vector<int> &preorder, const vector<int> &inorder) {
+        for (int i = 0; i < inorder.size(); i++) {
+            index[inorder[i]] = i; // 构建map映射inorder的值和标签，方便寻找根节点的位置
+        }
+        myBuildTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+    }
+};
---
+++ c++/longestPalindrome.cpp
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode-cn.com/problems/longest-palindromic-substring
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string str) {
+        int n = str.length();
+        vector<vector<int>> dp(n, vector<int>(n));
+        string ans;
+        for (int l = 0; l < n; l++) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = 1;
+                } else if (l == 1) {
+                    dp[i][j] = (str[i] == str[j]);
+                } else {
+                    dp[i][j] = (dp[i + 1][j - 1] == 1 && str[i] == str[j]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = str.substr(i, l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
